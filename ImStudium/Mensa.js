1f6c6d2a8200f67924367721084ad5f93ca7a0bf
Darian <81618482+darlirium@users.noreply.github.com>
2021-04-10 16:23:36 +0200

    Mensa-Auswahl hinzugefügt

--- ImStudium/Mensa.js
+++ ImStudium/Mensa.js
@@ -1,40 +1,132 @@
- //Import needed Libraries
- import React, {Component}from "react";
- import { Text, StyleSheet, View} from "react-native";
+//Import needed Libraries
+import React, {Component}from "react";
+import { Alert, Text, StyleSheet, TouchableOpacity, View} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 //Create Component
 
 class Mensa extends Component {
-    constructor(){
-        super();
-        this.state = {
-            title: "Bildschirm von Mensa",
-        }
-    }
     render (){
         return(
-            <View style={styles.menu}> 
-                 <Text style= {styles.text}>{this.state.title}</Text> 
+            <View style={styles.container}>
+                <ScrollView>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa Große Pause</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa TiHo-Tower</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa Caballus</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Hauptmensa</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Contine</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa Campus Linden</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa HMTMH</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Marktstand Hauptmensa</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Cafeteria Herrenhausen</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa PZH</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.mensaButton} 
+                        onPress={() => Alert.alert("Mensa-Auswahl", "Mensa")}
+                    >
+                        <View>
+                            <Text style={styles.mensaButtonText}>Mensa Blumhardtstraße</Text>
+                        </View>
+                    </TouchableOpacity>
+                </ScrollView>
             </View>
         )
     }
-
 }
 
 const styles = StyleSheet.create({
 
-        menu: {
-            backgroundColor: "#DFDFE6",
-            height: 80,
-            alignItems: 'center', //Horizontal Alignment
-            justifyContent: "center", //Vertical Alignment
-        },
-        text: {
-            fontSize: 25,
-            fontWeight: 'bold',
-        }
-
-
+    container: {
+        backgroundColor: "white",
+        flex: 1,
+        width: "100%",
+    },
+    mensaButton: {
+        width: "100%",
+        height: 70,
+        backgroundColor: "white",
+        padding: 15,
+        alignItems: "flex-start",
+        justifyContent: "center",
+        borderBottomColor: '#CCCCCC',
+        borderBottomWidth: .5,
+        borderTopColor: '#CCCCCC',
+        borderTopWidth: .5,
+    },
+    mensaButtonText: {
+        color: "black",
+        fontSize: 18,       
+    },
 
 });
  
