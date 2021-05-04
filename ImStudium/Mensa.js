01ba938f4ac399b5ae0582eae577ee2c75556e26
Darian <81618482+darlirium@users.noreply.github.com>
2021-05-04 12:55:58 +0200

    RSS-Feeds für nächste Woche eingefügt, Datumsformate angepasst, Designanpassungen

--- ImStudium/Mensa.js
+++ ImStudium/Mensa.js
@@ -8,17 +8,61 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import Moment from 'moment';
 
 var mensaListe = [ // hier weitere Parameter einfügen wie z.B. Öffnungszeiten, RSS-URLs etc.
-    { id: 1, name: "Mensa Große Pause", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=7&wann=1&format=rss", },
-    { id: 2, name: "Mensa TiHo-Tower", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=0&wann=1&format=rss", },
-    { id: 3, name: "Mensa Caballus", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=1&wann=1&format=rss", },
-    { id: 4, name: "Hauptmensa", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=2&wann=1&format=rss", },
-    { id: 5, name: "Contine", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=3&wann=1&format=rss", },
-    { id: 6, name: "Mensa Campus Linden", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=6&wann=1&format=rss", },
-    { id: 7, name: "Mensa HMTMH", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=8&wann=1&format=rss", },
-    { id: 8, name: "Marktstand Hauptmensa", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=9&wann=1&format=rss", },
-    { id: 9, name: "Cafeteria Herrenhausen", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=12&wann=1&format=rss", },
-    { id: 10, name: "Mensa Garbsen", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=13&wann=1&format=rss", },
-    { id: 11, name: "Mensa Blumhardtstraße", rssHeute: "https://www.stwh-portal.de/mensa/index.php?wo=14&wann=1&format=rss", },
+    { 
+        id: 1, name: "Mensa Große Pause",
+        rssDieseWoche: "http://www.stwh-portal.de/mensa/index.php?wo=7&wann=2&format=rss", 
+        rssNaechsteWoche: "http://www.stwh-portal.de/mensa/index.php?wo=7&wann=3&format=rss",
+    },
+    {
+         id: 2, name: "Mensa TiHo-Tower", 
+         rssDieseWoche: "http://www.stwh-portal.de/mensa/index.php?wo=0&wann=2&format=rss", 
+         rssNaechsteWoche: "http://www.stwh-portal.de/mensa/index.php?wo=0&wann=3&format=rss", 
+    },
+    {
+         id: 3, name: "Mensa Caballus", 
+         rssDieseWoche: "http://www.stwh-portal.de/mensa/index.php?wo=1&wann=2&format=rss", 
+         rssNaechsteWoche: "http://www.stwh-portal.de/mensa/index.php?wo=1&wann=3&format=rss", 
+    },
+    {
+         id: 4, name: "Hauptmensa", 
+         rssDieseWoche: "http://www.stwh-portal.de/mensa/index.php?wo=2&wann=2&format=rss", 
+         rssNaechsteWoche: "http://www.stwh-portal.de/mensa/index.php?wo=2&wann=3&format=rss", 
+    },
+    {
+         id: 5, name: "Contine", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=3&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=3&wann=3&format=rss", 
+    },
+    {
+         id: 6, name: "Mensa Campus Linden", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=6&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=6&wann=3&format=rss", 
+    },
+    {
+         id: 7, name: "Mensa HMTMH", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=8&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=8&wann=3&format=rss", 
+    },
+    {
+         id: 8, name: "Marktstand Hauptmensa", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=9&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=9&wann=3&format=rss", 
+    },
+    {
+         id: 9, name: "Cafeteria Herrenhausen", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=12&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=12&wann=3&format=rss", 
+    },
+    {
+         id: 10, name: "Mensa Garbsen", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=13&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=13&wann=3&format=rss", 
+    },
+    {
+         id: 11, name: "Mensa Blumhardtstraße", 
+         rssDieseWoche: "https://www.stwh-portal.de/mensa/index.php?wo=14&wann=2&format=rss", 
+         rssNaechsteWoche: "https://www.stwh-portal.de/mensa/index.php?wo=14&wann=3&format=rss", 
+    },
 ];
 
 class Mensa extends Component {
@@ -27,19 +71,24 @@ class Mensa extends Component {
         super();
         this.preprocessSpeiseplan = this.preprocessSpeiseplan.bind(this);
         this.RSS = this.RSS.bind(this);
+        this.rssNextWeek = this.rssNextWeek.bind(this);
+        Moment.locale('de');
         this.state = {
             feed: { title: "", items: [] },
+            feedNaechsteWoche: { title: "", items: [] },
             selectedMensa: 1,
             loading: false,
-            date: new Date(),
-            dateIn14Days: Moment(new Date()).add(14, "days"),
+            today: new Date(),
+            selDate: new Date(),
+            firstDayThisWeek: Moment(new Date).startOf("isoWeek").toDate(),
+            lastDayNextWeek: Moment(new Date).add(7, "d").endOf("isoWeek").toDate(),
             mode: 'date',
             show: false,
         };
     }
     
     RSS(value) {
-        return fetch((mensaListe.find(x => x.id === value).rssHeute))
+        return fetch((mensaListe.find(x => x.id === value).rssDieseWoche))
             .then((response) => response.text())
             .then((responseData) => rssParser.parse(responseData))
             .then((rss) => {
@@ -50,23 +99,68 @@ class Mensa extends Component {
             });
     }
 
+    rssNextWeek(value) {
+        return fetch((mensaListe.find(x => x.id === value).rssNaechsteWoche))
+            .then((response) => response.text())
+            .then((responseData) => rssParser.parse(responseData))
+            .then((rss) => {
+                this.setState(() => ({
+                    feedNaechsteWoche: rss,
+                    loading: false
+                }));
+            });
+    }
+
+    getdatespeiseplan(speiseplan) {
+        //console.log("-------------");
+        //console.log(speiseplan);
+        //console.log("+++++++++++++++")
+        
+        var condition = new RegExp(Moment(this.state.selDate).format("DD.MM.YYYY"));
+        var result = speiseplan.filter(function (item) {
+            return condition.test(item.datum);
+        });
+        if(typeof result === "undefined") {
+            result = "Geschlossen"
+        };
+        //console.log(result[0]);
+        return result[0];
+    }
+
     preprocessSpeiseplan(speiseplan) {
-        let sliceSpeiseplan = speiseplan[0].match(/<li>.*?<\/li>/g);
-        return sliceSpeiseplan.map((menuePunkt, id) => (
-            <TouchableOpacity
-                key={id}
-                style={styles.menuePunktStyle} 
-                onPress={() => alert("Essen ausgewählt.")}
-            >
-                <View>
-                    <Text style={styles.menuePunktText}>{menuePunkt}</Text>
-                </View>
-            </TouchableOpacity>
-        ));
+        let selectedSpeiseplan = this.getdatespeiseplan(speiseplan);
+        if(typeof selectedSpeiseplan !== "undefined") {
+            let sliceSpeiseplan = selectedSpeiseplan.gericht.match(/<li>.*?<\/li>/g);
+            return sliceSpeiseplan.map((menuePunkt, id) => (
+                <TouchableOpacity
+                    key={id}
+                    style={styles.menuePunktStyle} 
+                    onPress={() => alert("Essen ausgewählt.")}
+                >
+                    <View>
+                        <Text style={styles.menuePunktText}>{menuePunkt}</Text>
+                    </View>
+                </TouchableOpacity>
+                
+            ))
+        } else {
+            return (
+                <TouchableOpacity
+                    key={id}
+                    style={styles.menuePunktStyle} 
+                    onPress={() => alert("Essen ausgewählt.")}
+                >
+                    <View>
+                        <Text style={styles.menuePunktText}>Geschlossen</Text>
+                    </View>
+                </TouchableOpacity>
+            )
+        }
     }
 
     componentDidMount() {
         this.RSS(1);
+        this.rssNextWeek(1);
     }
     
     buildMensaPicker() {
@@ -88,14 +182,14 @@ class Mensa extends Component {
     changeMensa(value) {
         this.setState(() => ({ selectedMensa: value }));
         this.RSS(value);
+        this.rssNextWeek(value);
     }
     
     setDate = (event, selectedDate) => {
-        console.log("selectedDate=" + selectedDate);
-            const currentDate = selectedDate || this.state.date;
-            this.setState(() => ({ show: Platform.OS === 'ios' }));
-            this.setState(() => ({ date: currentDate }));
-      }
+        const currentDate = selectedDate || this.state.selDate;
+        this.setState(() => ({ show: Platform.OS === 'ios' }));
+        this.setState(() => ({ selDate: currentDate }));
+    }
 
     show = mode => {
         this.setState({ show: true, mode, });
@@ -107,14 +201,13 @@ class Mensa extends Component {
 
     buildDatePicker() {
         return (
-            <View style={styles.datePickerView}>
-                
+            <View style={styles.datePickerView}>               
                 <TouchableOpacity
                     style={styles.datePickerButton} 
                     onPress={this.datepicker}
                 >
                     <View>
-                        <Text style={styles.datePickerText}>{Moment(this.state.date).format("DD.MM.YYYY")}</Text>
+                        <Text style={styles.datePickerText}>{Moment(this.state.selDate).format("DD.MM.YYYY")}</Text>
                     </View>
                     <View style={styles.calendarIconView}>
                         <Image
@@ -126,11 +219,11 @@ class Mensa extends Component {
                 { this.state.show && (
                     <DateTimePicker
                         testID="dateTimePicker"
-                        value={this.state.date}
+                        value={this.state.selDate}
                         mode="date"
                         display="default"
-                        maximumDate={this.state.date}
-                        minimumDate={this.state.date}
+                        maximumDate={this.state.lastDayNextWeek}
+                        minimumDate={this.state.firstDayThisWeek}
                         onChange={this.setDate}
                     />
                 )}
@@ -139,11 +232,16 @@ class Mensa extends Component {
     }
     
     render (){
-        const speiseplanObject = { mensaID: this.state.selectedMensa, speiseplan: this.state.feed.items.map(item => item.description) };
-        
+        var speiseplanObject;
+
+        if(Moment(this.state.selDate).isSame(this.state.today, "isoWeek")) {
+            speiseplanObject = { mensaID: this.state.selectedMensa, speiseplan: this.state.feed.items.map(item => ({datum: item.title, gericht: item.description})) };
+        } else {
+            speiseplanObject = { mensaID: this.state.selectedMensa, speiseplan: this.state.feedNaechsteWoche.items.map(item => ({datum: item.title, gericht: item.description})) };
+        };
+         
         if(speiseplanObject.speiseplan.length > 0 && this.state.loading == false) {
             const plan = this.preprocessSpeiseplan(speiseplanObject.speiseplan);
-
             return(
                 <View style={styles.container}>
                     <View style={styles.PickerContainer}>
@@ -165,10 +263,9 @@ class Mensa extends Component {
                     </View>
                 </View>
             )
-        }
-        
+        }      
     }
-   }
+}
 
 const styles = StyleSheet.create({
 
